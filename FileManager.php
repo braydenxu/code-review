403df1aeaed28827209b58d8ccae7b3b4c20d6f6
braydenxu <maxxu_1214@hotmail.com>
2015-06-25 00:50:39 -0400

    For code review - php version

--- FileManager.php
+++ FileManager.php
@@ -26,7 +26,7 @@ class TreeNode {
  *    4  5     4
  */
 $root = new TreeNode(1);
-$root->left = new TreeNode(2);
+$root->left = new TreeNode(2); 
 $root->right = new TreeNode(3);
 $root->left->left = new TreeNode(4);
 $root->left->right = new TreeNode(5);
